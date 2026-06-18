17a94c3045aa5b3da9737b9b86f65e4004db972e
shihabsumon585 <shihabsumon585@gmail.com>
2026-06-18 11:01:19 +0600

    get data from the file database

--- learning-node/src/controler/products.controler.ts
+++ learning-node/src/controler/products.controler.ts
@@ -1,27 +1,30 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
+import { readProduct } from "../service/product.service";
 
 export const productController = (req: IncomingMessage, res: ServerResponse) => {
 
-    const productsItemList = [
-        {
-            id: 423,
-            name: "Energy Plus Biscuit",
-            price: 55,
-            size: "medium"
-        },
-        {
-            id: 234,
-            name: "Gold meri Biscuit",
-            price: 70,
-            size: "medium"
-        },
-        {
-            id: 709,
-            name: "Nutty Biscuit",
-            price: 15,
-            size: "small"
-        }
-    ]
+    // const productsItemList = [
+    //     {
+    //         id: 423,
+    //         name: "Energy Plus Biscuit",
+    //         price: 55,
+    //         size: "medium"
+    //     },
+    //     {
+    //         id: 234,
+    //         name: "Gold meri Biscuit",
+    //         price: 70,
+    //         size: "medium"
+    //     },
+    //     {
+    //         id: 709,
+    //         name: "Nutty Biscuit",
+    //         price: 15,
+    //         size: "small"
+    //     }
+    // ]
+
+    const productsItemList = readProduct();
 
     res.writeHead(200, { "content-type": "application/json" });
     res.end(JSON.stringify({
